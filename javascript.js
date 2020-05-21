869f4ce7bfe9a3a989009968d2cd5bcefaa8d611
omarkhalid1998 <52598101+omarkhalid1998@users.noreply.github.com>
2020-05-21 05:51:52 -0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.tab-link').click( function() {
+	
+	var tabID = $(this).attr('data-tab');
+	
+	$(this).addClass('active').siblings().removeClass('active');
+	
+	$('#tab-'+tabID).addClass('active').siblings().removeClass('active');
+});
+
+});
